1cd7866dbd69e0cd57d8054e131986de9df4551e
Rohit Thorbole <rohitthorbole4@gmail.com>
2026-09-19 20:16:15 +0530

    S3 module completed

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "this" {
+resource "aws_s3_bucket" "aws_s3_bucket" {
   bucket = var.bucket_name
 
   tags = {
@@ -7,16 +7,16 @@ resource "aws_s3_bucket" "this" {
   }
 }
 
-resource "aws_s3_bucket_versioning" "this" {
-  bucket = aws_s3_bucket.this.id
+resource "aws_s3_bucket_versioning" "s3_versioning" {
+  bucket = aws_s3_bucket.aws_s3_bucket.id
 
   versioning_configuration {
     status = "Enabled"
   }
 }
 
-resource "aws_s3_bucket_lifecycle_configuration" "this" {
-  bucket = aws_s3_bucket.this.id
+resource "aws_s3_bucket_lifecycle_configuration" "s3_lifecycle" {
+  bucket = aws_s3_bucket.aws_s3_bucket.id
 
   rule {
     id     = "invoice-lifecycle"
@@ -33,6 +33,64 @@ resource "aws_s3_bucket_lifecycle_configuration" "this" {
   }
 
   depends_on = [
-    aws_s3_bucket_versioning.this
+    aws_s3_bucket_versioning.s3_versioning
   ]
+}
+
+resource "aws_kms_key" "my_kms_key" {
+  description = "KMS key for S3 bucket encryption"
+  policy     = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:cloud_user"
+        }
+        Action = "kms:*"
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Principal = {
+          service = "s3.amazonaws.com"
+        }
+        Action = [
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_kms_alias" "s3" {
+  name          = "alias/my-s3-key"
+  target_key_id = aws_kms_key.my_kms_key.key_id
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "s3_kms_encryption" {
+  bucket = aws_s3_bucket.aws_s3_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
+      kms_master_key_id = aws_kms_key.my_kms_key.arn
+    }
+    bucket_key_enabled = true
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_public_access_block" {
+  bucket = aws_s3_bucket.aws_s3_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
